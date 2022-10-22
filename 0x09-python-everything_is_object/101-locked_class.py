02dea198a9e3f90697c39e18f04198b8bc19d4dd
Tolu Kay <lharsheh@gmail.com>
2022-10-22 15:43:55 +0100

    next task

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-"""
-Defines class with no class or object attribute
+"""Defines class with no class or object attribute"""
 
+
+class LockedClass():
+    """prevent user from creating new instance attribute dynamically unless attribute is "first_name"""
+__slots__ = "first_name"
